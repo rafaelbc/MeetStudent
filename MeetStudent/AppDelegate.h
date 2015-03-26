42e0d7da370cc2b2754f65a4db38789245b82d94
rafael.cabral <rcabral24ward.com.br>
2015-03-26 16:06:25 -0300

    Initial Commit

---
+++ MeetStudent/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeetStudent
+//
+//  Created by Rafael Cabral on 26/03/15.
+//  Copyright (c) 2015 BEPiD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MeetStudent/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MeetStudent
+//
+//  Created by Rafael Cabral on 26/03/15.
+//  Copyright (c) 2015 BEPiD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
